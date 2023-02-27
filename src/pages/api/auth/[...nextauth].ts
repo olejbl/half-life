cbdce9aa43c610bfa03a2827dff5b211825d5162
olejbl <38566764+olejbl@users.noreply.github.com>
2023-02-27 11:15:10 +0100

    db setup, tailwind plugin

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,26 @@
-import NextAuth from "next-auth";
-import { authOptions } from "~/server/auth";
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { env } from "~/env.mjs";
+import { prisma } from "~/server/db";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: env.GITHUB_ID,
+      clientSecret: env.GITHUB_SECRET,
+    }),
+  ],
+  // Include user.id on session
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+};
 
 export default NextAuth(authOptions);
